6472fd87b15338f5c839e3f563343bf3c38d1ac2
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-01-01 14:59:21 +0100

    restart preview each update

--- src/routes.rs
+++ src/routes.rs
@@ -6,7 +6,6 @@ use crate::rpi_cam::RpiCam;
 
 type MutexRpiCam = Arc<Mutex<RpiCam>>;
 
-
 #[get("/preview")]
 pub async fn preview(data: web::Data<MutexRpiCam>) -> HttpResponse {
     data.lock().unwrap().check_preview_status();
@@ -46,6 +45,8 @@ pub async fn set_height(data: web::Data<MutexRpiCam>, path: web::Path<u32>) -> H
     let mut rpi = data.lock().unwrap();
     (*rpi).height = *path;
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -55,6 +56,8 @@ pub async fn set_hflip(data: web::Data<MutexRpiCam>, path: web::Path<bool>) -> H
     let mut rpi = data.lock().unwrap();
     (*rpi).hflip = *path;
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -63,6 +66,8 @@ pub async fn set_vflip(data: web::Data<MutexRpiCam>, path: web::Path<bool>) -> H
     
     let mut rpi = data.lock().unwrap();
     (*rpi).vflip = *path;
+
+    rpi.restart_preview();
 
     HttpResponse::Ok().body("")
 }
@@ -84,6 +89,8 @@ pub async fn set_shutter_speed(data: web::Data<MutexRpiCam>, path: web::Path<u32
     let mut rpi = data.lock().unwrap();
     (*rpi).shutter_speed = *path;
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -92,6 +99,8 @@ pub async fn set_sharpness(data: web::Data<MutexRpiCam>, path: web::Path<i8>) ->
     
     let mut rpi = data.lock().unwrap();
     (*rpi).sharpness = *path;
+
+    rpi.restart_preview();
 
     HttpResponse::Ok().body("")
 }
@@ -102,6 +111,8 @@ pub async fn set_contrast(data: web::Data<MutexRpiCam>, path: web::Path<i8>) -> 
     let mut rpi = data.lock().unwrap();
     (*rpi).contrast = *path;
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -110,6 +121,8 @@ pub async fn set_brightness(data: web::Data<MutexRpiCam>, path: web::Path<i8>) -
     
     let mut rpi = data.lock().unwrap();
     (*rpi).brightness = *path;
+
+    rpi.restart_preview();
 
     HttpResponse::Ok().body("")
 }
@@ -120,6 +133,8 @@ pub async fn set_saturation(data: web::Data<MutexRpiCam>, path: web::Path<i8>) -
     let mut rpi = data.lock().unwrap();
     (*rpi).saturation = *path;
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -128,6 +143,8 @@ pub async fn set_iso(data: web::Data<MutexRpiCam>, path: web::Path<u16>) -> Http
     
     let mut rpi = data.lock().unwrap();
     (*rpi).iso = *path;
+
+    rpi.restart_preview();
 
     HttpResponse::Ok().body("")
 }
@@ -138,6 +155,8 @@ pub async fn set_stabilization(data: web::Data<MutexRpiCam>, path: web::Path<boo
     let mut rpi = data.lock().unwrap();
     (*rpi).stabilization = *path;
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -146,6 +165,8 @@ pub async fn set_ev_compensation(data: web::Data<MutexRpiCam>, path: web::Path<i
     
     let mut rpi = data.lock().unwrap();
     (*rpi).ev_compensation = *path;
+
+    rpi.restart_preview();
 
     HttpResponse::Ok().body("")
 }
@@ -156,6 +177,8 @@ pub async fn set_exposure(data: web::Data<MutexRpiCam>, path: web::Path<String>)
     let mut rpi = data.lock().unwrap();
     (*rpi).exposure = path.to_string();
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -164,6 +187,8 @@ pub async fn set_analog_gain(data: web::Data<MutexRpiCam>, path: web::Path<f32>)
     
     let mut rpi = data.lock().unwrap();
     (*rpi).analog_gain = *path;
+
+    rpi.restart_preview();
 
     HttpResponse::Ok().body("")
 }
@@ -174,6 +199,8 @@ pub async fn set_digital_gain(data: web::Data<MutexRpiCam>, path: web::Path<f32>
     let mut rpi = data.lock().unwrap();
     (*rpi).digital_gain = *path;
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -182,6 +209,8 @@ pub async fn set_awb(data: web::Data<MutexRpiCam>, path: web::Path<String>) -> H
     
     let mut rpi = data.lock().unwrap();
     (*rpi).awb = path.to_string();
+
+    rpi.restart_preview();
 
     HttpResponse::Ok().body("")
 }
@@ -192,6 +221,8 @@ pub async fn set_awb_blue(data: web::Data<MutexRpiCam>, path: web::Path<f32>) ->
     let mut rpi = data.lock().unwrap();
     (*rpi).awb_blue = *path;
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -200,6 +231,8 @@ pub async fn set_awb_red(data: web::Data<MutexRpiCam>, path: web::Path<f32>) -> 
     
     let mut rpi = data.lock().unwrap();
     (*rpi).awb_red = *path;
+
+    rpi.restart_preview();
 
     HttpResponse::Ok().body("")
 }
@@ -210,6 +243,8 @@ pub async fn set_effect(data: web::Data<MutexRpiCam>, path: web::Path<String>) -
     let mut rpi = data.lock().unwrap();
     (*rpi).effect = path.to_string();
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -219,6 +254,8 @@ pub async fn set_metering(data: web::Data<MutexRpiCam>, path: web::Path<String>)
     let mut rpi = data.lock().unwrap();
     (*rpi).metering = path.to_string();
 
+    rpi.restart_preview();
+
     HttpResponse::Ok().body("")
 }
 
@@ -227,6 +264,8 @@ pub async fn set_drc(data: web::Data<MutexRpiCam>, path: web::Path<String>) -> H
     
     let mut rpi = data.lock().unwrap();
     (*rpi).drc = path.to_string();
+
+    rpi.restart_preview();
 
     HttpResponse::Ok().body("")
 }
