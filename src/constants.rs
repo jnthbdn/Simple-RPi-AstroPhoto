74442c9ac15cb610669fed7313fd678773b02c38
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-01-02 18:51:14 +0100

    Add disable preview constant

--- src/constants.rs
+++ src/constants.rs
@@ -3,6 +3,7 @@ pub static FILENAME_PREVIEW : &str = "static/capture/preview.jpg";
 
 // Env. variables
 pub static ENV_SHOW_MMAL_ERROR : &str = "PRINT_MMAL_ERROR";
+pub static ENV_DISABLE_PREVIEW : &str = "DISABLE_PREVIEW";
 
 pub fn capture_path(filename : &str) -> String{
     format!("{}{}", PATH_SAVE_CAPTURE, filename)
